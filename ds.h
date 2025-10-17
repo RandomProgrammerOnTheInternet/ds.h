e3961c502e2f26355db010c1ae129186992c0936
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2025-10-17 18:44:52 -0400

    yeah its about time i updated this

--- ds.h
+++ ds.h
@@ -1,13 +1,26 @@
 #ifndef DS_H
 #define DS_H
-#endif // DS_H
 
 #include <stdlib.h>
 
 // Dynamic Arrays
-#define LIST(x) x*
-#define INIT_LIST(x, y) x = (typeof(x))malloc(y * sizeof(typeof(*x))); size_t x ## _size = y * sizeof(typeof(*x))
-#define LIST_RESIZE(x, y) x = (typeof(x))realloc(x, y * sizeof(typeof(*x))); x ## _size = y * sizeof(typeof(*x))
-#define LIST_ADD(x, y) LIST_RESIZE(x, (x ## _size / sizeof(typeof(*x)) + y))
-#define LIST_APPEND(x, y) LIST_ADD(x, 1); x[x ## _size / sizeof(typeof(*x)) - 1] = y
-#define LIST_FREE(x) if(x != NULL) free(x)
+#define NEW_LIST(x) struct list_ ## x {x* value; size_t length;}
+
+#define LIST(x) struct list_ ## x
+
+#define INIT_LIST(x, y) \
+x.length = y; \
+x.value = malloc(x.length * sizeof(typeof(x)));
+
+#define LIST_RESIZE(x, y) \
+x.length = y; \
+x.value = realloc(x.value, x.length * sizeof(typeof(x)))
+
+#define LIST_ADD(x, y) LIST_RESIZE(x, x.length + y)
+
+#define LIST_APPEND(x, y) LIST_ADD(x, 1); \
+x.value[x.length - 1] = y
+
+#define LIST_FREE(x) if(x.value != NULL) free(x.value)
+
+#endif // DS_H
